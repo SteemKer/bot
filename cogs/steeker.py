3ffd733c184c0b840f8bfa98180adc7552451ee2
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-02-22 10:52:12 +0530

    lsit packs

--- cogs/steeker.py
+++ cogs/steeker.py
@@ -8,7 +8,7 @@ class Steeker(commands.Cog):
         self.bot: SteekerBot = bot
         self.db = self.bot.database
 
-    @commands.group(name="pack", invoke_without_command=True)
+    @commands.group(name="pack", aliases=["packs"], invoke_without_command=True)
     @commands.is_owner()
     async def pack(self, ctx: commands.Context):
         """Pack command"""
@@ -27,6 +27,27 @@ class Steeker(commands.Cog):
         pack_id = await self.db.create_pack(str(ctx.author.id), name, ctx.message.id)
         await message.edit(
             content=f"**<a:stickbug:813271257759612938> | Successfully created a sticker pack with name `{name}` and ID `{pack_id}`**")
+        return
+
+    @pack.command(name="list")
+    @commands.is_owner()
+    @commands.guild_only()
+    async def packs_list(self, ctx: commands.Context):
+        """
+        Get a list of your pack with IDs
+        """
+
+        message = await ctx.send(f"<a:c4:813270233993183282> | Searching through my database")
+        packs = await self.db.get_user_packs(str(ctx.author.id))
+        if not packs or len(packs) <= 0:
+            await message.edit(
+                content=f"<:crypuddle:813275761230217246> | I couldn't find any packs owned by `{str(ctx.author)}`")
+            return
+        msg = "```"
+        for pack in packs:
+            msg += f"{pack['pack_id']} - {pack['name']}\n"
+        msg += "```"
+        await message.edit(content=msg)
         return
 
 
